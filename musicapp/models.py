c320c8e6dab663b01624d108f26083f852f0f466
Phenolah <104977409+Phenolah@users.noreply.github.com>
2022-11-03 05:48:53 -0700

    Update models.py

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,15 +1,40 @@
+from email.policy import default
+from turtle import title
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+
 class Artiste(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    age = models.PositiveIntegerField(null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, null=True)
+    age = models.IntegerField()
+
+    def getfullname(self):
+        return self.first_name + self.last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Song(models.Model):
-    tittle = models.ForeignKey(Artiste, on_delete=models.CASCADE)
-    date_released = models.DateField()
-    likes = models.IntegerField()
-    artiste_id = models.IntegerField(primary_key = True)
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField(blank=True)
+
+    def getSongTitle(self):
+        return self.title
+
+    def __str__(self):
+        return f"{self.title} - {self.artiste.getfullname()}"
+
+
 class Lyric(models.Model):
-    content = models.ForeignKey(Song, on_delete=models.CASCADE)
-    song_id = models.IntegerField(primary_key = True)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+    source = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"Lyrics for {self.song.artiste.getfullname()}'s {self.song.getSongTitle()} - {self.source}"
