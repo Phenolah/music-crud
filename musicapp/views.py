49d4dc41d56ea6ef7805f5bb495c2de1c4e5b29c
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-05-24 14:06:13 -0700

    Update views.py

--- musicapp/views.py
+++ musicapp/views.py
@@ -7,30 +7,50 @@
 
 
 from rest_framework import generics
-
 from .models import Artiste, Song, Lyric
 from .serializers import ArtisteSerializer, SongSerializer, LyricSerializer
 
+
+# Define your views here
+
 class SongList(generics.ListCreateAPIView):
+    """
+    API endpoint that allows the listing and creation of songs.
+    """
     queryset = Song.objects.all()
     serializer_class = SongSerializer
 
 class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint that allows the retrieval, update, and deletion of a specific song.
+    """
     queryset = Song.objects.all()
     serializer_class = SongSerializer
 
 class ArtisteList(generics.ListCreateAPIView):
+    """
+    API endpoint that allows the listing and creation of artistes.
+    """
     queryset = Artiste.objects.all()
     serializer_class = ArtisteSerializer
 
 class ArtisteDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint that allows the retrieval, update, and deletion of a specific artiste.
+    """
     queryset = Artiste.objects.all()
     serializer_class = ArtisteSerializer
 
 class LyricList(generics.ListCreateAPIView):
+    """
+    API endpoint that allows the listing and creation of lyrics.
+    """
     queryset = Lyric.objects.all()
     serializer_class = LyricSerializer
 
 class LyricDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint that allows the retrieval, update, and deletion of a specific lyric.
+    """
     queryset = Lyric.objects.all()
     serializer_class = LyricSerializer
